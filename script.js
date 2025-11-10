7cde31ea5fd021ce3745b47c8418a57081256ec3
jihun0821 <afgetre0213@gmail.com>
2025-11-10 19:18:47 +0900

    Fix cursor style in user nickname span

--- script.js
+++ script.js
@@ -392,7 +392,7 @@ function updateUI(user) {
         // Make the user info clickable to open privacy modal
         // We show "- 닉네임" but make the nickname portion clickable
         const displayNameOrEmail = user.displayName || user.email;
-        userInfo.innerHTML = `- <span id="userNickname" cursor: pointer;">${escapeHtml(displayNameOrEmail)}</span>`;
+        userInfo.innerHTML = `- <span id="userNickname" text-decoration: "cursor: pointer;">${escapeHtml(displayNameOrEmail)}</span>`;
         const nicknameSpan = document.getElementById('userNickname');
         if (nicknameSpan) {
             nicknameSpan.addEventListener('click', (e) => {
